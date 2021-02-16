57684d1eafd8ab274386fdd73479a52cbe283861
Jordon <jordontgarcia@gmail.com>
2021-02-15 20:09:03 -0800

    Added custom description conditions

---
+++ functions/conditions.js
@@ -0,0 +1,13 @@
+let weatherConditions = {
+    'clear sky': 'Clear Skies',
+    'broken clouds': 'Mostly Cloudy',
+    'scattered clouds': 'Partly Cloudy',
+    'light rain': 'Light Rain Showers',
+    'light snow': 'Light Snow Flurries',
+    'few clouds': 'Mostly Clear',
+    'overcast clouds': 'Cloudy',
+    'mist': 'Misty',
+    'heavy intensity rain': 'Rain Showers'
+}
+
+export default weatherConditions;
